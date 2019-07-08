a0a8746e59c4374f99eb961fe1d848bc99e2a7bc
Maxim.Gorbatyuk <maxim.gorbatyuk@outlook.com>
2019-07-08 22:52:02 +0600

    Have debugged ProductsService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductsService } from './services/products-service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { ProductListComponent } from './product-list/product-list.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    ProductsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/products-service.ts
+++ src/app/services/products-service.ts
@@ -1,5 +1,6 @@
 import { Product } from '../models/product';
 import { Injectable } from '@angular/core';
+import { ProductCategory } from '../models/product-category';
 
 Injectable({
     providedIn: 'root'
@@ -7,19 +8,20 @@ Injectable({
 export class ProductsService {
     getAll(): Product[] {
         return [
-            this.createProduct('Product 1', '', 100, true),
-            this.createProduct('Product 2', '', 200, true),
-            this.createProduct('Product 3', '', 150, false),
+            this.createProduct('Product 1', '', 100, true, ProductCategory.Car),
+            this.createProduct('Product 2', '', 200, true, ProductCategory.Hiking),
+            this.createProduct('Product 3', '', 150, false, ProductCategory.Tourism),
         ];
         
     }
 
-    private createProduct(name: string, description: string, price: number, isAvailable: boolean): Product {
+    private createProduct(name: string, description: string, price: number, isAvailable: boolean, category: ProductCategory): Product {
         const product = new Product();
         product.name = name;
         product.description = description;
         product.price = price;
         product.isAvailable = isAvailable;
+        product.category = category;
         return product;
     }
 }
